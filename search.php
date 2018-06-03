6a23c686294eb19951036285a58db03a91ee9c2f
9427 <31707686+9427@users.noreply.github.com>
2018-06-03 23:43:16 +0300

    Update search.php

--- search.php
+++ search.php
@@ -1,20 +1,24 @@
    <?php
    require_once 'connection.php';
  
-$link = mysqli_connect($host, $user, $password, $database) 
-or die ("Ошибка подключения к базе данных" . mysqli_error());
-$name = strtr($_GET['name'], '*', '%');
-$class = strtr($_GET['class'], '*', '%');
-echo "<form method='GET' action='search.php'>
-<p>Имя ученика: <input type='text' name='name' value='$name'></p>
-<p>Класс: <input type='text' name='class' value='$class'></p>
-<p><input type='submit' name='enter' value='Найти'></p>
-</form>";
-if (isset($_GET['enter']))
-{
-  $sql="SELECT mark_student, mark_class, mark_subject, mark_teacher, mark_value
-FROM mark
-WHERE mark_student LIKE '%$name%' AND mark_class LIKE '%$class%'";
+    $link = mysqli_connect($host, $user, $password, $database) 
+    or die ("Ошибка:" . mysqli_error());
+
+    $name = strtr($_GET['name'], '*', '%');
+    $subj = strtr($_GET['subj'], '*', '%');
+
+    echo "<form method='GET' action='search.php'>
+    <p>Введите имя ученика: <input type='text' name='name' value='$name'></p>
+    <p>Введите предмет: <input type='text' name='subj' value='$subj'></p>
+    <p><input type='submit' name='enter' value='Поиск'></p>
+    </form>";
+
+    if (isset($_GET['enter']))
+    {
+      $sql="SELECT mark_student, mark_class, mark_subject, mark_teacher, mark_value
+    FROM mark
+    WHERE mark_student LIKE '%$name%' AND mark_subject LIKE '%$subj%'";
+
 $result = mysqli_query($link, $sql);
 echo "<table border='1'>
 <tr> 
@@ -22,8 +26,7 @@ echo "<table border='1'>
 <th>mark_class</th>
 <th>mark_subject</th>
 <th>mark_teacher</th>
-<th>mark_value</th>
-</tr>";
+<th>mark_value</th></tr>";
 while($row = mysqli_fetch_array($result))
 {
 echo "<tr>";
@@ -34,7 +37,12 @@ echo "<td>" . $row['mark_teacher'] . "</td>";
 echo "<td>" . $row['mark_value'] . "</td>";
 echo "</tr>";
 }
+
 echo "</table>"; 
+
 }
+
+
 mysqli_close($link);
 ?>
+
