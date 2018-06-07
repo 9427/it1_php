aad2a1ba1beaf8d8f4ff7fc9190bfc96133ca097
9427 <31707686+9427@users.noreply.github.com>
2018-06-07 10:03:52 +0300

    Update search.php

--- search.php
+++ search.php
@@ -3,17 +3,19 @@
  
     $link = mysqli_connect($host, $user, $password, $database) 
     or die ("Ошибка:" . mysqli_error());
+    $name = strtr($_GET['name'], '*', '%');
+    $subj = strtr($_GET['subj'], '*', '%');
+	 $name1 = strtr($_GET['name'], '*', '*');
+    $subj1 = strtr($_GET['subj'], '*', '*');
 
     echo "<form method='GET' action='search.php'>
-    <p>Введите имя ученика: <input type='text' name='name' value='$name'></p>
-    <p>Введите предмет: <input type='text' name='subj' value='$subj'></p>
+    <p>Введите имя ученика: <input type='text' name='name' placeholder='$name1' value='$name'></p>
+    <p>Введите предмет: <input type='text' name='subj' placeholder='$subj1' value='$subj'></p>
     <p><input type='submit' name='enter' value='Поиск'></p>
     </form>";
 
     if (isset($_GET['enter']))
     {   
-      $name = strtr($_GET['name'], '*', '%');
-      $subj = strtr($_GET['subj'], '*', '%');
       $sql="SELECT mark_student, mark_class, mark_subject, mark_teacher, mark_value
     FROM mark
     WHERE mark_student LIKE '%$name%' AND mark_subject LIKE '%$subj%'";
